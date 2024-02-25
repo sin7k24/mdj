ef911f1ce762c48f9e291ff60969f2db0a62f19a
Shingo Nakanishi <sin7k24@hotmail.com>
2024-02-25 19:44:58 +0900

    done.

--- src/app/edit-page/edit-page.component.ts
+++ src/app/edit-page/edit-page.component.ts
@@ -1,5 +1,5 @@
 import { HttpClient } from '@angular/common/http';
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
     selector: 'app-edit-page',
@@ -13,6 +13,8 @@ export class EditPageComponent {
 
     mdContent = '';
 
+    @ViewChild('editor') editor!: ElementRef;
+
     constructor(private http: HttpClient) {}
 
     readMarkdown(event: any) {
@@ -21,10 +23,12 @@ export class EditPageComponent {
         this.day = event.day;
 
         console.log(event);
-        this.http.get(`api/v1/md/${event.year}/${event.month}/${event.day}`, { responseType: 'text' }).subscribe((res) => {
-            console.log(res);
-            this.mdContent = res.length != 0 ? res : "まだ日記がありません";
-        });
+        this.http
+            .get(`api/v1/md/${event.year}/${event.month}/${event.day}`, { responseType: 'text' })
+            .subscribe((res) => {
+                console.log(res);
+                this.mdContent = res.length != 0 ? res : 'まだ日記がありません';
+            });
     }
 
     saveMarkdown() {
@@ -32,10 +36,36 @@ export class EditPageComponent {
             year: this.year,
             month: this.month,
             day: this.day,
-            mdContent: this.mdContent
+            mdContent: this.mdContent,
         };
-        this.http.post(`api/v1/md`, body).subscribe((res)=>{
+        this.http.post(`api/v1/md`, body).subscribe((res) => {
             console.log(res);
-        });        
+        });
+    }
+
+    dragOver(evt: DragEvent) {
+        evt.preventDefault();
+        console.log('drag', evt);
+    }
+
+    drop(evt: DragEvent) {
+        evt.preventDefault();
+        console.log('drop', evt);
+        const file = evt.dataTransfer!.files[0];
+        console.log(file);
+
+        const formData = new FormData();
+        formData.append('upload_file', file);
+
+        this.http.post(`api/v1/img`, formData, { responseType: 'text' }).subscribe((res) => {
+            console.log(res);
+            const editor = this.editor.nativeElement;
+            // カーソルがまだ当たっていない場合は末尾になる
+            console.log("editor.selectionStart", editor.selectionStart);
+            this.mdContent =
+                editor.value.substr(0, editor.selectionStart) +
+                `![](${res})` +
+                editor.value.substr(editor.selectionStart);
+        });
     }
 }
